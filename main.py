43c799dfd97572d2da15ebc515aff5eb51582183
treetree17 <knyuan17@outlook.com>
2023-05-18 17:51:19 +0800

    第一版(完成用户主界面部分功能）

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 
 import database
 
+database.init_database()
+
 CONFIG = {
     "host": '127.0.0.1:1433',
     "user": 'sa',
@@ -16,6 +18,8 @@ if conn:
     print("连接成功!")
 cursor = conn.cursor()
 app = Flask(__name__)
+uid = "user"
+cid = "cat"
 
 
 class Login():
@@ -31,13 +35,11 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-
         userid = request.form.get('UID')
         username = request.form.get('Uname')
         password = request.form.get('password')
         department = request.form.get('department')
         major = request.form.get('major')
-
         info1 = {
             'UID': userid,
             'UNAME': username,
@@ -45,21 +47,20 @@ def register():
             'DEPARTMENT': department,
             'MAJOR': major,
         }
+
         ans = database.signup(info1)
         if ans:
-            print('成功')
-            # 注册成功跳转到登录页面
             return render_template('login.html')
     else:
-        # 否则依然返回注册页面
-        print('注册失败')
         return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global uid
     if request.method == 'POST':
         userid = request.form.get('UID')
+        uid = userid
         password = request.form.get('password')
         info = {
             'ID': userid,
@@ -71,6 +72,7 @@ def login():
             return render_template('widget_1.html')
         if user['class'] == 'user':
             print('登录成功！')
+            print(uid)
             return render_template('widget_2.html')
     else:
         print('登录失败！')
@@ -84,6 +86,28 @@ def my_endpoint1():
 @app.route('/my-endpoint2', methods=['POST'])
 def my_endpoint2():
     return render_template('register.html')
+
+
+@app.route('/apply', methods=['POST'])
+def apply():
+    global cid
+    rowId_param = request.form.get('rowId')
+    cid = rowId_param
+    res = database.apply_cat(cid, uid)
+    print(res)
+    return "已提交申请"
+
+
+@app.route('/cat')
+def cat():
+    rows1 = database.cat_info()
+    return render_template('widget_2.html', rows1=rows1)
+
+
+@app.route('/apply_info')
+def apply_info():
+    rows2 = database.get_apply(uid)
+    return render_template('widget_2.html', rows2=rows2)
 
 
 app.run(port=8080)
